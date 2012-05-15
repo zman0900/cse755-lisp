a1b446da5d9341d5521cb33addf54ed2105079d2
Dan Ziemba <zman0900@gmail.com>
2012-05-15 13:10:11 -0400

    Add symbols

--- src/com/cse755/LispParser.java
+++ src/com/cse755/LispParser.java
@@ -7,13 +7,40 @@ package com.cse755;
  */
 public class LispParser {
 
-	private LispTokenizer tk;
+	public enum Symbols {
+		// Terminal symbols
+		TS_ATOM, TS_DOT, TS_C_PAREN, TS_O_PAREN, TS_INVALID,
+		// Non-terminal symbols
+		NTS_E, NTS_R, NTS_S, NTS_X, NTS_Y;
+		
+		public static Symbols lexer(Token t) {
+			switch (t.type) {
+			case NUMBER:
+				return TS_ATOM;
+			case WORD:
+				return TS_ATOM;
+			case DOT:
+				return TS_DOT;
+			case CLOSE_PAREN:
+				return TS_C_PAREN;
+			case OPEN_PAREN:
+				return TS_O_PAREN;
+			default:
+				return TS_INVALID;  // Should never reach this
+			}
+		}
+	}
 
+
+	private LispTokenizer tk;
+	
 	/**
 	 * 
 	 */
 	public LispParser(LispTokenizer tk) {
 		this.tk = tk;
 	}
+	
+	
 
 }
